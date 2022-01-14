eef654a56e146be58f1d96cf27329273aeace274
gabiroll <gabrielgarcia1153gabi@gmail.com>
2022-01-14 11:52:33 +0100

    se agrega el proyecto

---
+++ src/Ud3/Ud3ComentarioVariasLineas.java
@@ -0,0 +1,14 @@
+package Ud3;
+
+public class Ud3ComentarioVariasLineas {
+	public static void main(String args[]){
+		/*System.out.println("Hola");
+		System.out.println("Adios");
+		System.out.println("Cucu");*/
+		for(int i=0;i<6;/*ahora viene el paso */ i++)
+			System.out.print(i+" ");
+		System.out.println("FIN DE PROGRAMA");
+	}
+}
+
+
